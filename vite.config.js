2861c6dc8e94ac546a5da8bf4620940ac8c38ed7
Mike-OC <rid-mc@mail.ru>
2023-10-06 01:52:13 +0400

    change vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: '/Portfolio-on-vue/',
+//  publicPath: '/Portfolio-on-vue/',
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/Portfolio-on-vue/'
+  : '/',
   plugins: [vue()]
 })
